9bf3af6e73206fd6820c2e9eb919d0783fa69863
Rahul Sharma <rahulsharma.hps@gmail.com>
2025-12-06 19:09:49 +0530

    Fix: Fully working build for both C++ and Python packages

---
+++ src/semantic_vision_pkg/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'semantic_vision_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Rahul',
+    maintainer_email='rahul@todo.todo',
+    description='YOLO Vision Node',
+    license='TODO',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'vision_node = semantic_vision_pkg.vision_node:run_vision_node',
+        ],
+    },
+)
